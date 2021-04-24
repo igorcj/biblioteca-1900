2aedd3192a2b3db5fe0884fa4858dec5e54b2867
jpdonasolo <jpdonasolo@gmail.com>
2021-04-24 13:21:40 -0300

    Adicionada tabela Emprestimos

--- core.py
+++ core.py
@@ -7,6 +7,7 @@ from sqlalchemy import (
     Date,
     Column,
     ForeignKey,
+    ForeignKeyConstraint
 )
 from sqlalchemy.orm import (
     relationship,
@@ -34,7 +35,7 @@ class Livro(Base):
     disponivel = Column(Boolean, default=True)
 
     # Relações
-    aluno_id = Column(Integer, ForeignKey("aluno.ID"))
+    aluno_ID = Column(Integer, ForeignKey("aluno.ID"))
     dono = relationship("Aluno", back_populates="livros")
 
     def __repr__(self):
@@ -58,3 +59,35 @@ class Aluno(Base):
 
     def __repr__(self):
         return f"Aluno(nome={self.nome}, quarto={self.nome}, telefone={self.telefone})"
+
+
+class Emprestimo(Base):
+
+    __tablename__ = "emprestimo"
+
+    ID = Column(Integer, primary_key=True)
+
+    # Alunos
+    locatario_ID = Column(Integer, ForeignKey("aluno.ID"))
+    locador_ID = Column(Integer, ForeignKey("aluno.ID"))
+
+    # Livro
+    livro_academico = Column(Boolean)
+    livro_letra = Column(String(1))
+    livro_indice = Column(Integer)
+
+    # Dados
+    data_emp = Column(Date, nullable=True)
+    data_dev = Column(Date, nullable=True)
+
+    # Relações
+    dono = relationship("Aluno", foreign_keys=[locatario_ID])
+    locatario = relationship("Aluno", foreign_keys=[locador_ID])
+    livro = relationship("Livro", foreign_keys=[livro_academico, livro_letra, livro_indice])
+
+    __table_args__ = (ForeignKeyConstraint([livro_academico, livro_letra, livro_indice],
+                                           [Livro.academico, Livro.letra, Livro.indice]), {})
+    
+
+    def __repr__(self):
+        return f"Emprestimo(dono={self.dono.nome}, locatario={self.locatario.nome}, livro={self.livro.titulo})"
