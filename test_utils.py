b81b74b29d37feeb82ac88d2ff9b83bc132d4938
jpdonasolo <jpdonasolo@gmail.com>
2021-04-30 18:44:28 -0300

    Funçao para Emprestimos e testes

---
+++ test_utils.py
@@ -0,0 +1,41 @@
+import pytest
+import utils as u
+
+
+def test_aluno_errado():
+    matriculas = ["b41309", "B4130", "134543", "34354", "B41309d", "B413H5"]
+
+    for matricula in matriculas:
+        assert u.verify_st_code(matricula) == False
+
+
+def test_aluno_certo():
+    matriculas = ["B41309", "B41303", "B34543"]
+
+    for matricula in matriculas:
+        assert u.verify_st_code(matricula) == True
+
+
+def test_livro_errado():
+    codigos = ["A-F-1", "0 F-1", "0-c-2", "0-F-0"]
+    for codigo in codigos:
+        assert u.verify_bk_code(codigo) == False
+
+
+def test_livro_errado():
+    codigos = ["1-F-1", "3-H-7"]
+    for codigo in codigos:
+        assert u.verify_bk_code(codigo) == True
+
+
+def test_find_successor():
+    IDs = [1, 1, 2, 3, 4]
+
+    with pytest.raises(u.IDError):
+        u.find_successor(IDs)
+
+    IDs = []
+    assert u.find_successor(IDs) == 1
+
+    IDs = [1, 2, 4, 5, 7, 8, 9]
+    assert u.find_successor(IDs) == 3
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-from core import Livro, Aluno, Emprestimo
 import inspect
+import datetime
+from core import Livro, Aluno, Emprestimo
+from string import ascii_uppercase
 
 
 class IDError(Exception):
@@ -37,10 +39,10 @@ def find_next_ID(session=None, academico: bool = None, letra: str = None) -> int
     for livro in livros:
         IDs.append(livro.indice)
 
-    return find_sucessor(IDs)
+    return find_successor(IDs)
 
 
-def find_sucessor(IDs: list) -> int:
+def find_successor(IDs: list) -> int:
     """
     Retorna o sucessor do menor número cujo sucessor não está na lista
     """
@@ -59,3 +61,77 @@ def find_sucessor(IDs: list) -> int:
             return IDs[pos] + 1
 
     return IDs[-1] + 1
+
+
+def verify_st_code(st_code: str) -> bool:
+
+    try:
+        assert len(st_code) == 6
+        _ = int(st_code[1:])
+        cond = st_code[0] == 'B'
+        assert cond == True
+
+    except (ValueError, TypeError, AssertionError):
+        return False
+
+    return True
+
+
+def verify_bk_code(bk_code: str) -> bool:
+
+    try:
+        assert len(bk_code.split("-")) == 3
+
+        categoria, letra, indice = bk_code.split("-")
+        categoria, indice = int(categoria), int(indice)
+
+        assert indice != 0
+
+        cond = all([isinstance(categoria, int),
+                   letra in ascii_uppercase, isinstance(indice, int)])
+        assert cond == True
+
+    except (AssertionError, TypeError, ValueError):
+        return False
+
+    return True
+
+
+def fazer_emprestimo(session=None, st_code: str = None, bk_code: str = None):
+    """
+    Faz um empréstimo
+
+    Parametros
+    ----------
+    st_code : string
+        string no formato "B00000"
+
+    bk_code : string
+        string do ID do livro no formato C-L-00    
+    """
+
+    if not (verify_bk_code(bk_code) and verify_st_code(st_code)):
+        raise ValueError("session, matrícula ou código do livro inválido(s)")
+
+    categoria, letra, indice = bk_code.split("-")
+    letra = letra.upper()
+    categoria, indice = int(categoria), int(indice)
+
+    livro = session.query(Livro).filter_by(
+        categoria=categoria, letra=letra, indice=indice)
+    locatario = session.query(Aluno).filter_by(matricula=st_code).first()
+
+    if livro is None:
+        raise ValueError("Livro não encontrado")
+
+    if locatario is None:
+        raise ValueError("Locatário não encontrado")
+
+    dono = livro.dono
+    emprestimo = Emprestimo(dono=dono, locatario=locatario,
+                            livro=livro, data_emp=datetime.datetime.today())
+
+    session.add(emprestimo)
+    session.commit()
+
+    return True
