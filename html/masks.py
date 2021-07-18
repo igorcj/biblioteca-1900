1b70902e8eb7927ca7c5459eab8a5cdd0bdd3d82
Igor Cortes Junqueira <igor@pop-os.localdomain>
2021-07-18 09:55:30 -0300

    modal box 6

--- html/masks.py
+++ html/masks.py
@@ -144,9 +144,9 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
